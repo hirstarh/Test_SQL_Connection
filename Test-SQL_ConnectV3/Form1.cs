e3891094107878e4806c71ff059129c44e0d668a
hirstarh@gmail.com <andrew.hirst1@nhs.net>
2021-12-31 15:31:10 +0000

    Add project files.

---
+++ Test-SQL_ConnectV3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_SQL_ConnectV3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection cnn;
+
+            /* SQL Server IP address 45.76.138.73 */
+            connectionString = @"Data Source=agem-se1.agem-bisenhs.org.uk;Initial Catalog =Master;User ID=AHirst;Password=Coniston125";
+
+            cnn = new SqlConnection(connectionString);
+
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Congratulations you have successfully connected to the SQL-SE1 server", "Connection Made", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cnn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("No connection made to AGEM-SE1 SQL server, probably Firewall issues", "Connection Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+        }
+    }
+}
